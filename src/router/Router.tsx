ae8841e5d18b6313ee01c406c5b07185d21c6270
ka52k <76809740+ka52k@users.noreply.github.com>
2021-03-23 14:18:08 +0900

    20210323－2変更

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,8 @@
 import { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Login } from "../components/pages/Login";
+import { homeRoutes } from "./HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
 
 export const Router: VFC = memo(() => {
   return (
@@ -8,7 +10,24 @@ export const Router: VFC = memo(() => {
       <Route exact path="/">
         <Login />
       </Route>
-      
+      <Route 
+        path="/home" 
+        render={( {match: { url }}) => (
+          <Switch>
+            {homeRoutes.map((route) => (
+              <Route 
+                key={route.path} 
+                exact={route.exact} 
+                path={`${url}${route.path}`}>
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )} 
+      />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
-  )
+  );
 });
